725e6fca6e33c8d93444146b010ce50df25c09d8
darthsticky <tristancarter@gmail.com>
2018-07-18 19:02:11 -0400

    Finished Update of Base FE

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,2 +1,23 @@
 import React from 'react';
 import axios from 'axios';
+// import relevant material-ui components
+// look to material docs for more.follows @material-ui/ syntax
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      default: false // replace
+    }
+  }
+
+  render() {
+    return
+    <div id="container">
+      {/* structure here should be layout based call to other components to populate. */}
+      !!!APP LAYOUT GOES HERE USE MATERIAL!!!
+    </div>
+  }
+}
+
+export default App;
