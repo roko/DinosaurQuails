2ac2797651183c57b9ed0ed23de62e22e54539ae
darthsticky <tristancarter@gmail.com>
2018-07-19 19:00:15 -0400

    quick config fix.

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   module : {
     rules : [
       {
-        test : /\.jsx/,
+        test : /\.jsx?/,
         include : SRC_DIR,
         loader : 'babel-loader',
         query: {
