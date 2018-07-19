9f0b9ce1f83fcb5ddf1f2a19b9fbfd67489abd9e
roko <forthrightro@gmail.com>
2018-07-19 11:48:24 -0700

    corrected jobs to require app from index  instead

--- server/jobs.js
+++ server/jobs.js
@@ -1,11 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const db = require('../db/index.js')
-const app = express();
+const db = require('../db/index.js');
+const app = require('./index.js');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+
 
 app.get('/jobs', function (req, res) {
   //render '/jobs'and current 'job' instances
