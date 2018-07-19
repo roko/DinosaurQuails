98c14191f0803ade8151393d279a61d0da4dd224
Ningyi <ningyi6@gmail.com>
2018-07-18 17:18:26 -0700

    add db schemas and models

--- db/index.js
+++ db/index.js
@@ -4,17 +4,48 @@ mongoose.connect('mongodb://localhost/test');
 const db = mongoose.connection;
 
 db.on('error', function() {
-    console.log('error')
+  console.log('error');
 });
 
-db.once('open', function () {
-    console.log('Server connected')
+db.once('open', function() {
+  console.log('Server connected');
 });
 
-//TODO: ADD SCHEMAS
+//TODO: ADD USER SCHEMA
+let userSchema = mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  userName: String,
+  email: String,
+  password: String,
+  jobs: [job]
+});
 
-//TODO: ADD MODELS
+//TODO: ADD USER MODEL
+let User = mongoose.model('User', userSchema);
 
-//EXPORT MODELS 
+// ADD JOB SCHEMA
+let jobSchema = mongoose.Schema({
+  userId: String,
+  company: {
+    name: String,
+    jobTitle: String,
+    webSite: String
+  },
+  contact: {
+    email: String,
+    phone: String,
+    recruiter: String
+  },
+  postDate: Date,
+  appliedDate: Date,
+  interviewedDate: Date,
+  coverLetterUrl: String
+});
+
+// ADD JOB MODEL
+let Job = mongoose.model('Job', jobSchema);
+
+//EXPORT MODELS
 
 module.exports.db = db;
