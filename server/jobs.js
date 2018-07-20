bc03b1ce227263adbac265a364769a6bb3cb7c2f
jamesvertica <james.vertica@gmail.com>
2018-07-19 17:17:54 -0700

    Created '/job' helper functions w/ Ro

--- server/jobs.js
+++ server/jobs.js
@@ -4,28 +4,41 @@ const app = require('./index.js');
 const bluebird = require('bluebird');
 const jobs = express.Router();
 
+const jobHelperDisplay = (err) => {
+  if(err) console.log('Issue Rendering all jobs, check DB: ', err);
+   res.send(db.Jobs.find()); 
+}
 const jobHelperQuery = (err, searchTerms) => {
-  // if (err) {
-  //   console.log(err);
-  // }
-  // //get all jobs (no filter terms) from db
+  if (err) console.log('Job Query Error: ', err);
+  res.send(db.Jobs.find(searchTerms));
+  // get specific elements from db
   // //pass terms into find
+<<<<<<< HEAD
   // res.send()
+=======
+}
+>>>>>>> Created '/job' helper functions w/ Ro
 
 const jobHelperSaver = (err, inputtedTerms) => {
-  // if (err) {
-  //   console.log(err);
-  // }
-//takes req .contact, etc - creates new instance entry into db
-//test this
-//save update
+  let terms = inputtedTerms || 'none';
+  if (err) console.log('Job Saver Error: ', err);
+  // db.createJob(inputtedTerms); implement in DB
+  res.send('Job Saved!');
+//TODO:
+  //input calender modal into date 
+  //test this
 }
 
-jobs.get('/jobs', jobHelperQuery);
-  //render '/jobs'and current 'job' instances
+jobs.get('/jobs', jobHelperDisplay);
+//render '/jobs'and current 'job' instances
 
 jobs.post('/jobs', jobHelperSaver);
-  //send req.miscFields to DB for new instance
+//send req.miscFields to DB for new instance
 
+<<<<<<< HEAD
 module.exports = jobs;
 
+=======
+module.exports = jobs; 
+module.exports.jobHelperQuery = jobHelperQuery;
+>>>>>>> Created '/job' helper functions w/ Ro
