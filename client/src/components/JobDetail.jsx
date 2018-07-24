a16210f7640238872c5622dc8013fe8de2398004
jamesvertica <35894238+jamesvertica@users.noreply.github.com>
2018-07-24 09:17:20 -0500

    added axios

--- client/src/components/JobDetail.jsx
+++ client/src/components/JobDetail.jsx
@@ -4,6 +4,7 @@ import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Modal from '@material-ui/core/Modal';
 import Button from '@material-ui/core/Button';
+import axios from 'axios';
 
 
 function rand() {
