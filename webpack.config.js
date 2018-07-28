cd1f6f930833d7cbeed50a5f5e736fda13cd5a89
Tristan Carter <tristancarter@gmail.com>
2018-07-27 20:52:46 -0400

    Another Edit for Webpack Config Deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/client/dist');
+var SRC_DIR = path.join(__dirname, "client", "src");
+var DIST_DIR = path.join(__dirname, "client", "dist");
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
@@ -15,7 +15,7 @@ module.exports = {
         test : /\.jsx?/,
         include : SRC_DIR,
         loader : 'babel-loader',
-        query: {
+        options: {
           presets: ['env', 'react']
        }
       }
